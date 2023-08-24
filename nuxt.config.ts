495e647272364ccbef81f556e59d794561009c53
Am.ChrisKE <dev.humblechris254@gmail.com>
2023-08-24 07:32:06 +0000

    added tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@sidebase/nuxt-auth"],
+  modules: ["@sidebase/nuxt-auth", "@nuxtjs/tailwindcss"],
   auth: {
     provider: {
       type: "authjs",
     },
-    globalAppMiddleware: true
+    globalAppMiddleware: true,
   },
   runtimeConfig: {
     public: {
       GITHUB_CLIENT_ID: process.env.NUXT_GITHUB_CLIENT_ID,
-      GITHUB_CLIENT_SECRET: process.env.NUXT_GITHUB_CLIENT_SECRET
+      GITHUB_CLIENT_SECRET: process.env.NUXT_GITHUB_CLIENT_SECRET,
     },
   },
+  tailwindcss : {
+    cssPath: '~/assets/css/tailwind.css'
+  }
 });
