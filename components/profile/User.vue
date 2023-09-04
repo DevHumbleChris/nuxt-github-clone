57a9bd31fe323888e8c53b58bf4726b6fe0e36fd
DevHumbleChris <dev.humblechris254@gmail.com>
2023-09-04 21:15:27 +0300

    saving changes

--- components/profile/User.vue
+++ components/profile/User.vue
@@ -9,5 +9,22 @@ const userInfo = computed(() => {
 </script>
 
 <template>
-  <section>user</section>
+  <section class="bg-light-dark">
+    <div class="grid grid-cols-8">
+        <div class="col-span-2 p-5 space-y-3 text-gray-200">
+            <div class="max-w-64 lg:w-64 h-auto mx-auto rounded-full border-2 border-gray-400 shadow">
+                <nuxt-img :src="userInfo?.avatar_url" :alt="userInfo?.login" class="w-full h-full object-cover rounded-full" />
+            </div>
+            <div class="space-y-4">
+                <div>
+                    <h2 class="text-2xl text-white font-semibold">{{ userInfo?.name }}</h2>
+                    <h1 class="text-gray-500">{{ userInfo?.login }}</h1>
+                </div>
+                <button class="block bg-[#21262c] p-2 w-full rounded-md">follow</button>
+            </div>
+            <p>{{ userInfo?.bio }}</p>
+        </div>
+        <div class="col-span-6 bg-blue-400 p-5">hello</div>
+    </div>
+  </section>
 </template>
