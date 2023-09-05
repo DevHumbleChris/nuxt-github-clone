9a858af638e8889d17d004d17fda3974678d6663
DevHumbleChris <dev.humblechris254@gmail.com>
2023-09-05 19:59:36 +0300

    successfully updated the EventsRemarks component

--- components/dashboard/EventRemarks.vue
+++ components/dashboard/EventRemarks.vue
@@ -11,18 +11,38 @@ const eventType = computed(() => {
 const userEvent = computed(() => {
   return props?.userEvent;
 });
+
+const { data: repoInfo } = await getRepoInfo(userEvent?.value?.repo?.name);
 </script>
 
 <template>
-  <div v-if="eventType === 'starred'" class="text-gray-100">
+  <NuxtLink
+    target="_blank"
+    :to="repoInfo?.html_url"
+    v-if="eventType === 'starred'"
+    class="block text-gray-100"
+  >
     {{ userEvent?.repo?.name }}
-  </div>
+  </NuxtLink>
   <div v-else-if="eventType === 'created'">a repository</div>
-  <div v-else-if="eventType === 'released'" class="text-gray-100">{{ userEvent?.payload?.release?.name }}</div>
-  <div v-else-if="eventType === 'forked'" class="text-gray-100">
+  <div v-else-if="eventType === 'released'" class="text-gray-100">
+    {{ userEvent?.payload?.release?.name }}
+  </div>
+  <NuxtLink
+    target="_blank"
+    :to="repoInfo?.html_url"
+    v-else-if="eventType === 'forked'"
+    class="text-gray-100"
+  >
     {{ userEvent?.repo?.name }}
-  </div>
-  <div v-else-if="eventType === 'added'" class="text-gray-100">
-    {{ userEvent?.payload?.member?.login }} <span class="text-gray-500">as a collaborator</span>
-  </div>
+  </NuxtLink>
+  <NuxtLink
+    :to="userEvent?.payload?.member?.login"
+    target="_blank"
+    v-else-if="eventType === 'added'"
+    class="text-gray-100"
+  >
+    {{ userEvent?.payload?.member?.login }}
+    <span class="text-gray-500">as a collaborator</span>
+  </NuxtLink>
 </template>
